2e5bca0cf73462ca33f34ffe3109a5103b9b1f22
rkrahj@gmail.com <rkrahj@gmail.com>
2021-08-02 11:06:52 +0000

    Add project files.

---
+++ WpfMMP2/LiveChart.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using LiveCharts;
+using LiveCharts.Wpf;
+
+namespace WpfMMP2
+{
+    /// <summary>
+    /// Interaction logic for LiveChart.xaml
+    /// </summary>
+    public partial class LiveChart : UserControl
+    {
+        public SeriesCollection SeriesCollection { get; set; }
+        public LiveChart()
+        {
+            InitializeComponent();
+            SeriesCollection = new SeriesCollection
+                {
+                new LineSeries
+                {
+                    Title = "Pleth",
+                    Values = new ChartValues<double> { 4, 6, 5, 2 ,4 },
+                    PointGeometry= null,
+                    Stroke= Brushes.Green
+
+
+                }
+            };
+
+        }
+    }
+}
---
+++ WpfMMP2/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfMMP2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void StackPanel_Loaded(object sender, RoutedEventArgs e)
+        {
+            RenderChart.Children.Clear();
+            RenderChart.Children.Add(new LiveChart());
+            //RenderChart1.Children.Clear();
+            //RenderChart1.Children.Add(new LiveChart());
+            //RenderChart2.Children.Clear();
+            //RenderChart2.Children.Add(new LiveChart());
+            //RenderChart3.Children.Clear();
+            //RenderChart3.Children.Add(new LiveChart());
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            RenderChart.Children.Clear();
+            RenderChart.Children.Add(new LiveChart());
+            //RenderChart1.Children.Clear();
+            //RenderChart1.Children.Add(new LiveChart());
+            //RenderChart2.Children.Clear();
+            //RenderChart2.Children.Add(new LiveChart());
+            //RenderChart3.Children.Clear();
+            //RenderChart3.Children.Add(new LiveChart());
+        }
+    }
+}
